c342ed06c4fe31566d470eedeeab1e60372d6f21
Ashutosh Dubey <85245914+Ashutosh-Dubey18@users.noreply.github.com>
2022-07-23 22:16:05 +0530

    Backend Done

---
+++ Backend/chatroom-springboot-websocket/src/main/java/com/app/chatroom/chatroomspringbootwebsocket/models/Message.java
@@ -0,0 +1,37 @@
+package com.app.chatroom.chatroomspringbootwebsocket.models;
+
+
+public class Message {
+	
+	private String name;
+	
+	private String content;
+	
+	public Message(String name, String content) {
+		super();
+		this.name = name;
+		this.content = content;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Message [name=%s, content=%s]", name, content);
+	}
+	
+}
