48cc377cf507f8f1d576d361ded070a16049e91e
Ashutosh Dubey <85245914+Ashutosh-Dubey18@users.noreply.github.com>
2022-07-23 22:16:56 +0530

    Frontend Done

---
+++ Frontend/chat-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Messenger from './componenet/Messenger';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Messenger></Messenger>
+    </div>
+  );
+}
+
+export default App;
